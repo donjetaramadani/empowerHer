bc37290f371e9dd2050aad739a565255a18ebd9e
Donjeta Ramadani <donjetaramadani90@gmail.com>
2024-04-27 13:25:41 +0300

    Initial commit

--- App.js
+++ App.js
@@ -1,11 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, Button, StatusBar } from 'react-native'; // Import StatusBar from react-native
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import RegisterScreen from './screens/register';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: 'Home' }}
+        />
+        <Stack.Screen
+          name="Register"
+          component={RegisterScreen}
+          options={{ title: 'Register' }}
+        />
+      </Stack.Navigator>
+      <StatusBar style="auto" /> 
+    </NavigationContainer>
+  );
+}
+
+function HomeScreen({ navigation }) {
+  return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Button
+        title="Go to Register Screen"
+        onPress={() => navigation.navigate('Register')}
+      />
     </View>
   );
 }
---
+++ screens/register.js
@@ -0,0 +1,201 @@
+import React from "react";
+import {
+  StyleSheet,
+  ImageBackground,
+  Dimensions,
+  StatusBar,
+  KeyboardAvoidingView,
+} from "react-native";
+import { Block, Checkbox, Text, Button, Input } from "galio-framework";
+import { Images, Icon } from "../constants";
+
+
+const { width, height } = Dimensions.get("screen");
+
+class Register extends React.Component {
+    render() {
+      return (
+        <Block flex middle>
+          <StatusBar hidden />
+          <ImageBackground
+            source={Images.RegisterBackground}
+            style={{ width, height, zIndex: 1 }}
+          >
+            <Block safe flex middle>
+              <Block style={styles.registerContainer}>
+                <Block flex={0.25} middle style={styles.socialConnect}>
+                  <Text color="#8898AA" size={12}>
+                    Sign up with
+                  </Text>
+                  <Block row>
+                    <Button
+                      style={{ ...styles.socialButtons, marginRight: 30 }}
+                    >
+                      <Text style={styles.socialTextButtons}>GitHub</Text>
+                    </Button>
+                    <Button style={styles.socialButtons}>
+                      <Text style={styles.socialTextButtons}>Google</Text>
+                    </Button>
+                  </Block>
+                </Block>
+                <Block flex>
+                  <Block flex={0.17} middle>
+                    <Text color="#8898AA" size={12}>
+                      Or sign up the classic way
+                    </Text>
+                  </Block>
+                  <Block flex center>
+                    <KeyboardAvoidingView
+                      style={{ flex: 1 }}
+                      behavior="padding"
+                      enabled
+                    >
+                      <Block width={width * 0.8} style={{ marginBottom: 15 }}>
+                        <Input
+                          borderless
+                          placeholder="Name"
+                          iconContent={
+                            <Icon
+                              size={16}
+                              color="#ADB5BD"
+                              name="user"
+                              family="Font-Awesome"
+                              style={styles.inputIcons}
+                            />
+                          }
+                        />
+                      </Block>
+                      <Block width={width * 0.8} style={{ marginBottom: 15 }}>
+                        <Input
+                          borderless
+                          placeholder="Email"
+                          iconContent={
+                            <Icon
+                              size={16}
+                              color="#ADB5BD"
+                              name="envelope-o"
+                              family="Font-Awesome"
+                              style={styles.inputIcons}
+                            />
+                          }
+                        />
+                      </Block>
+                      <Block width={width * 0.8}>
+                        <Input
+                          password
+                          borderless
+                          placeholder="Password"
+                          iconContent={
+                            <Icon
+                              size={16}
+                              color="#ADB5BD"
+                              name="lock"
+                              family="Font-Awesome"
+                              style={styles.inputIcons}
+                            />
+                          }
+                        />
+                        <Block row style={styles.passwordCheck}>
+                          <Text size={12} color={"black"}>
+                            password strength:
+                          </Text>
+                          <Text bold size={12} color={"black"}>
+                            {" "}
+                            strong
+                          </Text>
+                        </Block>
+                      </Block>
+                      <Block row width={width * 0.75}>
+                        <Checkbox
+                          checkboxStyle={{
+                            borderWidth: 3,
+                          }}
+                          color={"black"}
+                          label="I agree with the"
+                        />
+                        <Button
+                          style={{ width: 100 }}
+                          color="transparent"
+                          textStyle={{
+                            color: "black",
+                            fontSize: 14,
+                          }}
+                        >
+                          Privacy Policy
+                        </Button>
+                      </Block>
+                      <Block middle>
+                        <Button color="primary" style={styles.createButton}>
+                          <Text
+                            bold
+                            size={14}
+                            color={"white"}
+                          >
+                            CREATE ACCOUNT
+                          </Text>
+                        </Button>
+                      </Block>
+                    </KeyboardAvoidingView>
+                  </Block>
+                </Block>
+              </Block>
+            </Block>
+          </ImageBackground>
+        </Block>
+      );
+    }
+  }
+const styles = StyleSheet.create({
+  registerContainer: {
+    width: width * 0.9,
+    height: height * 0.875,
+    backgroundColor: "#F4F5F7",
+    borderRadius: 4,
+    shadowColor: "black",
+    shadowOffset: {
+      width: 0,
+      height: 4,
+    },
+    shadowRadius: 8,
+    shadowOpacity: 0.1,
+    elevation: 1,
+    overflow: "hidden",
+  },
+  socialConnect: {
+    backgroundColor: "#fff",
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    borderColor: "#8898AA",
+  },
+  socialButtons: {
+    width: 120,
+    height: 40,
+    backgroundColor: "#fff",
+    shadowColor: "black",
+    shadowOffset: {
+      width: 0,
+      height: 4,
+    },
+    shadowRadius: 8,
+    shadowOpacity: 0.1,
+    elevation: 1,
+  },
+  socialTextButtons: {
+    color: "black",
+    fontWeight: "800",
+    fontSize: 14,
+  },
+  inputIcons: {
+    marginRight: 12,
+  },
+  passwordCheck: {
+    paddingLeft: 15,
+    paddingTop: 13,
+    paddingBottom: 30,
+  },
+  createButton: {
+    width: width * 0.5,
+    marginTop: 25,
+  },
+});
+
+export default Register;
